f0c81bc9ab5a810e720cb7736d69a854a31c05ff
Sosthène Guédon <sosthene@nitrokey.com>
2026-07-09 17:47:55 +0200

    Add alternate function configuration

--- components/stm32n657-hal/src/gpio.rs
+++ components/stm32n657-hal/src/gpio.rs
@@ -3,7 +3,7 @@
 use core::{convert::Infallible, marker::PhantomData};
 
 use embedded_hal::digital::v2::{InputPin, OutputPin};
-use stm32n6::stm32n657::{GPIOC_S, GPIOG_S};
+use stm32n6::stm32n657::{GPIOA_S, GPIOB_S, GPIOC_S, GPIOD_S, GPIOE_S, GPIOG_S, GPIOH_S};
 
 use crate::rcc::{Peripheral, Rcc};
 
@@ -20,7 +20,7 @@ pub struct Output<M> {
     _marker: PhantomData<M>,
 }
 
-pub struct Alternate<M> {
+pub struct Alternate<M, const F: u8> {
     _marker: PhantomData<M>,
 }
 
@@ -28,7 +28,7 @@ pub struct PushPull;
 
 macro_rules! impl_gpio {
     ($gpio:ident, $GPIO:ident, [
-        $($pin:ident: $Pin:ident = ($mode:ident, $ot:ident, $pupd:ident, $id:ident, $bs:ident, $br:ident),)*
+        $($pin:ident: $Pin:ident = ($mode:ident, $ot:ident, $pupd:ident, $id:ident, $bs:ident, $br:ident, $afr:ident, $afsel:ident),)*
     ]) => {
         pub struct $gpio {
             $(
@@ -51,13 +51,13 @@ macro_rules! impl_gpio {
         }
 
         $(
-            impl_pin!($GPIO, $Pin, $mode, $ot, $pupd, $id, $bs, $br);
+            impl_pin!($GPIO, $Pin, $mode, $ot, $pupd, $id, $bs, $br, $afr, $afsel);
         )*
     }
 }
 
 macro_rules! impl_pin {
-    ($GPIO:ident, $pin:ident, $mode:ident, $ot:ident, $pupd:ident, $id:ident, $bs:ident, $br:ident) => {
+    ($GPIO:ident, $pin:ident, $mode:ident, $ot:ident, $pupd:ident, $id:ident, $bs:ident, $br:ident, $afr:ident, $afsel:ident) => {
         pub struct $pin<M> {
             _marker: PhantomData<M>,
         }
@@ -113,7 +113,10 @@ macro_rules! impl_pin {
                 }
             }
 
-            pub fn into_alternate_pull_up(self) -> $pin<Alternate<PullUp>> {
+            pub fn into_alternate_pull_up<const F: u8>(self) -> $pin<Alternate<PullUp, F>> {
+                const {
+                    assert!(F <= 0xF);
+                };
                 // mode: 10 = alternate function
                 self.gpio()
                     .moder()
@@ -122,6 +125,9 @@ macro_rules! impl_pin {
                 self.gpio()
                     .pupdr()
                     .modify(|_, w| unsafe { w.$pupd().bits(0b10) });
+                self.gpio()
+                    .$afr()
+                    .modify(|_, w| unsafe { w.$afsel().bits(F) });
 
                 $pin {
                     _marker: Default::default(),
@@ -163,43 +169,60 @@ macro_rules! impl_pin {
     };
 }
 
-impl_gpio!(GpioA, GPIOC_S, [
-    a0: PinA0 = (mode0, ot0, pupd0, id0, bs0, br0),
-    a4: PinA4 = (mode4, ot4, pupd4, id4, bs4, br4),
+impl_gpio!(GpioA, GPIOA_S, [
+    a0: PinA0 = (mode0, ot0, pupd0, id0, bs0, br0, afrl,afsel0),
+    a4: PinA4 = (mode4, ot4, pupd4, id4, bs4, br4, afrl,afsel4),
 ]);
-impl_gpio!(GpioB, GPIOC_S, [
-    b4: PinB4 = (mode4, ot4, pupd4, id4, bs4, br4),
-    b5: PinB5 = (mode5, ot5, pupd5, id5, bs5, br5),
-    b8: PinB8 = (mode8, ot8, pupd8, id8, bs8, br8),
-    b9: PinB9 = (mode9, ot9, pupd9, id9, bs9, br9),
-    b13: PinB13 = (mode13, ot13, pupd13, id13, bs13, br13),
+impl_gpio!(GpioB, GPIOB_S, [
+    b4: PinB4 = (mode4, ot4, pupd4, id4, bs4, br4, afrl,afsel4),
+    b5: PinB5 = (mode5, ot5, pupd5, id5, bs5, br5, afrl,afsel5),
+    b8: PinB8 = (mode8, ot8, pupd8, id8, bs8, br8, afrh,afsel8),
+    b9: PinB9 = (mode9, ot9, pupd9, id9, bs9, br9, afrh,afsel9),
+    b13: PinB13 = (mode13, ot13, pupd13, id13, bs13, br13, afrh,afsel13),
 ]);
 impl_gpio!(GpioC, GPIOC_S, [
-    c0: PinC0 = (mode0, ot0, pupd0, id0, bs0, br0),
-    c1: PinC1 = (mode1, ot1, pupd1, id1, bs1, br1),
-    c2: PinC2 = (mode2, ot2, pupd2, id2, bs2, br2),
-    c3: PinC3 = (mode3, ot3, pupd3, id3, bs3, br3),
-    c4: PinC4 = (mode4, ot4, pupd4, id4, bs4, br4),
-    c5: PinC5 = (mode5, ot5, pupd5, id5, bs5, br5),
-    c6: PinC6 = (mode6, ot6, pupd6, id6, bs6, br6),
-    c7: PinC7 = (mode7, ot7, pupd7, id7, bs7, br7),
-    c13: PinC13 = (mode13, ot13, pupd13, id13, bs13, br13),
+    c0: PinC0 = (mode0, ot0, pupd0, id0, bs0, br0, afrl,afsel0),
+    c1: PinC1 = (mode1, ot1, pupd1, id1, bs1, br1, afrl,afsel1),
+    c2: PinC2 = (mode2, ot2, pupd2, id2, bs2, br2, afrl,afsel2),
+    c3: PinC3 = (mode3, ot3, pupd3, id3, bs3, br3, afrl,afsel3),
+    c4: PinC4 = (mode4, ot4, pupd4, id4, bs4, br4, afrl,afsel4),
+    c5: PinC5 = (mode5, ot5, pupd5, id5, bs5, br5, afrl,afsel5),
+    c6: PinC6 = (mode6, ot6, pupd6, id6, bs6, br6, afrl,afsel6),
+    c7: PinC7 = (mode7, ot7, pupd7, id7, bs7, br7, afrl,afsel7),
+    c13: PinC13 = (mode13, ot13, pupd13, id13, bs13, br13, afrh,afsel13),
 ]);
-impl_gpio!(GpioD, GPIOG_S, [
-    d0: PinD0 = (mode0, ot0, pupd0, id0, bs0, br0),
-    d2: PinD2 = (mode2, ot2, pupd2, id2, bs2, br2),
-    d5: PinD5 = (mode5, ot5, pupd5, id5, bs5, br5),
+impl_gpio!(GpioD, GPIOD_S, [
+    d0: PinD0 = (mode0, ot0, pupd0, id0, bs0, br0, afrl,afsel0),
+    d2: PinD2 = (mode2, ot2, pupd2, id2, bs2, br2, afrl,afsel2),
+    d5: PinD5 = (mode5, ot5, pupd5, id5, bs5, br5, afrl,afsel5),
 ]);
-impl_gpio!(GpioE, GPIOG_S, [
-    e0: PinE0 = (mode0, ot0, pupd0, id0, bs0, br0),
-    e4: PinE4 = (mode4, ot4, pupd4, id4, bs4, br4),
+impl_gpio!(GpioE, GPIOE_S, [
+    e0: PinE0 = (mode0, ot0, pupd0, id0, bs0, br0, afrl,afsel0),
+    e4: PinE4 = (mode4, ot4, pupd4, id4, bs4, br4, afrl,afsel4),
 ]);
 impl_gpio!(GpioG, GPIOG_S, [
-    g0: PinG0 = (mode0, ot0, pupd0, id0, bs0, br0),
-    g8: PinG8 = (mode8, ot8, pupd8, id8, bs8, br8),
-    g10: PinG10 = (mode10, ot10, pupd10, id10, bs10, br10),
+    g0: PinG0 = (mode0, ot0, pupd0, id0, bs0, br0, afrl,afsel0),
+    g8: PinG8 = (mode8, ot8, pupd8, id8, bs8, br8, afrh,afsel8),
+    g10: PinG10 = (mode10, ot10, pupd10, id10, bs10, br10, afrh,afsel10),
 ]);
-impl_gpio!(GpioH, GPIOG_S, [
-    h0: PinH0 = (mode0, ot0, pupd0, id0, bs0, br0),
-    h9: PinH9 = (mode9, ot9, pupd9, id9, bs9, br9),
+impl_gpio!(GpioH, GPIOH_S, [
+    h0: PinH0 = (mode0, ot0, pupd0, id0, bs0, br0, afrl,afsel0),
+    h9: PinH9 = (mode9, ot9, pupd9, id9, bs9, br9, afrh,afsel9),
 ]);
+
+pub const GPIO_ALTERNATE_FUNCTION_0: u8 = 0x0000000;
+pub const GPIO_ALTERNATE_FUNCTION_1: u8 = 0x0000001;
+pub const GPIO_ALTERNATE_FUNCTION_2: u8 = 0x0000002;
+pub const GPIO_ALTERNATE_FUNCTION_3: u8 = 0x0000003;
+pub const GPIO_ALTERNATE_FUNCTION_4: u8 = 0x0000004;
+pub const GPIO_ALTERNATE_FUNCTION_5: u8 = 0x0000005;
+pub const GPIO_ALTERNATE_FUNCTION_6: u8 = 0x0000006;
+pub const GPIO_ALTERNATE_FUNCTION_7: u8 = 0x0000007;
+pub const GPIO_ALTERNATE_FUNCTION_8: u8 = 0x0000008;
+pub const GPIO_ALTERNATE_FUNCTION_9: u8 = 0x0000009;
+pub const GPIO_AFLTERNATE_FUNCTION10: u8 = 0x000000A;
+pub const GPIO_AFLTERNATE_FUNCTION11: u8 = 0x000000B;
+pub const GPIO_AFLTERNATE_FUNCTION12: u8 = 0x000000C;
+pub const GPIO_AFLTERNATE_FUNCTION13: u8 = 0x000000D;
+pub const GPIO_AFLTERNATE_FUNCTION14: u8 = 0x000000E;
+pub const GPIO_AFLTERNATE_FUNCTION15: u8 = 0x000000F;
